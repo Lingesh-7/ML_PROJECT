a989e39223a3a8a5e781e50f8b1caae195ec37a0
Lingesh-7 <lingesh.r.official@gmail.com>
2026-01-24 11:33:52 +0530

    update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,23 +1,23 @@
 import logging
-
 import os
-
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_path= os.path.join(os.getcwd(), "logs",LOG_FILE)
+# Logs directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-os.makedirs(log_path,exist_ok=True)
+# Full log file path
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
-
+# Logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format= "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
 )
 
-# if __name__=='__main__':
-#     logging.info("Logging hhas started")
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
