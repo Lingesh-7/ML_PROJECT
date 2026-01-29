c368891dc37077c7f80750594971a8621a0b1eb6
Lingesh-7 <lingesh.r.official@gmail.com>
2026-01-29 21:32:02 +0530

    Flask web app

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,21 +39,80 @@ class ModelTrainer:
 
             models = {
                 "Linear Regression": LinearRegression(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Random Forest Regressor": RandomForestRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "AdaBoost": AdaBoostRegressor(),
+                "K-Neighbors": KNeighborsRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoostRegressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor()
+                # "CatBoostRegressor": CatBoostRegressor(verbose=False)
             }
+
+
+            params = {
+
+                "Linear Regression": {
+                    "fit_intercept": [True, False],
+                    "positive": [True, False]
+                },
+
+                "Decision Tree": {
+                    "criterion": ["squared_error", "friedman_mse"],
+                    "max_depth": [None, 5, 10, 20, 30],
+                    # "min_samples_split": [2, 5, 10],
+                    # "min_samples_leaf": [1, 2, 4]
+                },
+
+                "Random Forest": {
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [None, 10, 20, 30],
+                    # "min_samples_split": [2, 5, 10],
+                    # "min_samples_leaf": [1, 2, 4],
+                    # "max_features": ["sqrt", "log2"]
+                },
+
+                "Gradient Boosting": {
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    # "max_depth": [3, 5, 7],
+                    # "subsample": [0.8, 1.0]
+                },
+
+                "AdaBoost": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1, 1.0]
+                },
+
+                "K-Neighbors": {
+                    "n_neighbors": [3, 5, 7, 9, 11],
+                    "weights": ["uniform", "distance"],
+                    "algorithm": ["auto", "ball_tree", "kd_tree"]
+                },
+
+                "XGBRegressor": {
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    # "max_depth": [3, 5, 7],
+                    # "subsample": [0.8, 1.0],
+                    # "colsample_bytree": [0.8, 1.0]
+                },
+
+                # "CatBoostRegressor": {
+                #     "iterations": [300, 500],
+                #     "learning_rate": [0.03, 0.1],
+                #     # "depth": [6, 8, 10],
+                #     # "l2_leaf_reg": [1, 3, 5]
+                # }
+            }
+
 
             model_report = evaluate_model(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=params
             )
 
             best_model_name = max(model_report, key=model_report.get)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exceptions import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -20,22 +21,47 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+
+
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            param_grid = params.get(model_name)
+            if param_grid is None:
+                continue
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=param_grid,
+                cv=3,
+                
+            )
+
+            gs.fit(X_train, y_train)
+            print(gs.best_params_)
+
+            best_model = gs.best_estimator_
+
+            y_test_pred = best_model.predict(X_test)
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
+            models[model_name] = best_model
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
